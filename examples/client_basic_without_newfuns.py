38f677cada74fe718ebaf5c87ea21f6cd466e29c
Christian lange <christian.lange@mni.thm.de>
2021-01-28 19:48:19 +0100

    added result wrappers

--- examples/client_basic_without_newfuns.py
+++ examples/client_basic_without_newfuns.py
@@ -6,19 +6,60 @@ import clientApi
 from intermediateApi import ffi, lib
 
 client = clientApi.UaClient()
-retval = client.connect(b"opc.tcp://stellaluna:4840/")
-node = ffi.new("UA_NodeId*")
-node.namespaceIndex = ffi.cast("UA_UInt16", 1)
+retval = client.connect(b"opc.tcp://christian-ThinkPad:4840/")
 
-node.identifierType = lib.UA_NODEIDTYPE_NUMERIC
-node.identifier.numeric = ffi.cast("UA_UInt32", 42)
+# node 1
+node1 = ffi.new("UA_NodeId*")
+node1.namespaceIndex = ffi.cast("UA_UInt16", 1)
+node1.identifierType = lib.UA_NODEIDTYPE_NUMERIC
+node1.identifier.numeric = ffi.cast("UA_UInt32", 42)
 
-new_node = ffi.new("UA_NodeId*")
 
 qualified_name = ffi.new("UA_QualifiedName*")
 qualified_name.namespaceIndex = ffi.cast("UA_UInt16", 1)
-name = ffi.new("UA_String*")
+name = ffi.new("UA_String*", None)
 name.length = ffi.cast("size_t", 5)
 name.data = ffi.cast("UA_Byte*", 4)
-qualified_name.name = name
+qualified_name.name = name[0]
 
+# node 2
+node2 = ffi.new("UA_NodeId*")
+node2.namespaceIndex = ffi.cast("UA_UInt16", 1)
+node2.identifierType = lib.UA_NODEIDTYPE_NUMERIC
+node2.identifier.numeric = ffi.cast("UA_UInt32", 42)
+
+
+qualified_name2 = ffi.new("UA_QualifiedName*")
+qualified_name2.namespaceIndex = ffi.cast("UA_UInt16", 1)
+name2 = ffi.new("UA_String*", None)
+name2.length = ffi.cast("size_t", 5)
+name2.data = ffi.cast("UA_Byte*", 4)
+qualified_name2.name = name[0]
+
+
+# browse name
+qualified_name3 = ffi.new("UA_QualifiedName*")
+qualified_name3.namespaceIndex = ffi.cast("UA_UInt16", 1)
+name3 = ffi.new("UA_String*", None)
+name3.length = ffi.cast("size_t", 5)
+name3.data = ffi.cast("UA_Byte*", 4)
+qualified_name3.name = name[0]
+
+
+# node 4
+node4 = ffi.new("UA_NodeId*")
+node4.namespaceIndex = ffi.cast("UA_UInt16", 1)
+node4.identifierType = lib.UA_NODEIDTYPE_NUMERIC
+node4.identifier.numeric = ffi.cast("UA_UInt32", 42)
+
+
+qualified_name4 = ffi.new("UA_QualifiedName*")
+qualified_name4.namespaceIndex = ffi.cast("UA_UInt16", 1)
+name4 = ffi.new("UA_String*", None)
+name4.length = ffi.cast("size_t", 5)
+name4.data = ffi.cast("UA_Byte*", 4)
+qualified_name4.name = name[0]
+
+
+
+client.add_variable_node(node1[0], node2[0], node2[0], qualified_name3[0], node4[0])
--- src/api/clientApi.py
+++ src/api/clientApi.py
@@ -1,5 +1,5 @@
 from intermediateApi import ffi, lib
-import client_response_wrappers
+import client_response_wrappers as wrap
 
 class DefaultAttributes:
 
@@ -30,108 +30,108 @@ class UaClient:
 	def read_node_id_attribute(self, node_id):
 		out_node_id = ffi.new("UA_NodeId**")
 		status_code = lib.UA_Client_readNodeIdAttribute(self.ua_client, node_id, out_node_id)
-		return Read_node_id_attribute(status_code, out_node_id)
+		return wrap.Read_node_id_attribute(status_code, out_node_id)
 
 	def read_node_class_attribute(self, node_id):
 		out_node_id = ffi.new("UA_NodeClass**")
 		status_code = lib.UA_Client_readNodeClassAttribute(self.ua_client, node_id, out_node_class)
-		return Read_node_class_attribute(status_code, out_node_id)
+		return wrap.Read_node_class_attribute(status_code, out_node_id)
 
 	def read_browse_name_attribute(self, node_id):
 		out_browse_name = ffi.new("UA_QualifiedName**")
 		status_code = lib.UA_Client_readBrowseNameAttribute(self.ua_client, node_id, out_browse_name);
-		return Read_browse_name_attribute_wrapper(status_code, out_browse_name)
+		return wrap.Read_browse_name_attribute_wrapper(status_code, out_browse_name)
 
 	def read_display_name_attribute(self, node_id):
 		out_display_name = ffi.new("UA_LocalizedText**")
 		status_code = lib.UA_Client_readDisplayNameAttribute(self.ua_client, node_id, out_display_name)
-		return Read_display_name_attribute_wrapper(status_code, out_display_name)
+		return wrap.Read_display_name_attribute_wrapper(status_code, out_display_name)
 
 	def read_description_attribute(self, node_id):
 		out_description = ffi.new("UA_LocalizedText**")
 		status_code = lib.UA_Client_readDescriptionAttribute(self.ua_client, node_id, out_description)
-		return Read_description_attribute_wrapper(status_code, out_description)
+		return wrap.Read_description_attribute_wrapper(status_code, out_description)
 
 	def read_write_mask_attribute(self, node_id):
 		out_write_mask = ffi.new("UA_UInt32**")
 		status_code = lib.UA_Client_readWriteMaskAttribute(self.ua_client, node_id, out_write_mask)
-		return Read_write_mask_attribute_wrapper(status_code, out_write_mask)
+		return wrap.Read_write_mask_attribute_wrapper(status_code, out_write_mask)
 
 	def read_user_write_mask_attribute(self, node_id):
 		out_user_write_mask = ffi.new("UA_UInt32**")
 		status_code = lib.UA_Client_readUserWriteMaskAttribute(self.ua_client, node_id, out_user_write_mask)
-		return Read_user_write_mask_attribute_wrapper(status_code, out_user_write_mask)
+		return wrap.Read_user_write_mask_attribute_wrapper(status_code, out_user_write_mask)
 
 	def read_is_abstract_attribute(self, node_id):
 		out_is_abstract = ffi.new("UA_Boolean**")
 		status_code = lib.UA_Client_readIsAbstractAttribute(self.ua_client, node_id, out_is_abstract)
-		return Read_is_abstract_attribute_wrapper(status_code, out_is_abstract)
+		return wrap.Read_is_abstract_attribute_wrapper(status_code, out_is_abstract)
 
 	def read_symmetric_attribute(self, node_id):
 		out_symmetric = ffi.new("UA_Boolean**")
 		status_code = lib.UA_Client_readSymmetricAttribute(self.ua_client, node_id, out_symmetric)
-		return Read_symmetric_attribute_wrapper(status_code, out_symmetric)
+		return wrap.Read_symmetric_attribute_wrapper(status_code, out_symmetric)
 
 	def read_inverse_name_attribute(self, node_id):
 		out_inverse_name = ffi.new("UA_LocalizedText**")
 		status_code = lib.UA_Client_readInverseNameAttribute(self.ua_client, node_id, out_inverse_name)
-		return Read_inverse_name_attribute_wrapper(status_code, out_inverse_name)
+		return wrap.Read_inverse_name_attribute_wrapper(status_code, out_inverse_name)
 
 	def read_contains_no_loops_attribute(self, node_id):
 		out_contains_no_loops = ffi.new("UA_Boolean**")
 		status_code = lib.UA_Client_readContainsNoLoopsAttribute(self.ua_client, node_id, out_contains_no_loops)
-		return Read_contains_no_loops_attribute_wrapper(status_code, out_contains_no_loops)
+		return wrap.Read_contains_no_loops_attribute_wrapper(status_code, out_contains_no_loops)
 
 	def read_event_notifier_attribute(self, node_id):
 		out_event_notifier = ffi.new("UA_Byte**")
 		status_code = lib.UA_Client_readEventNotifierAttribute(self.ua_client, node_id, out_event_notifier)
-		return Read_event_notifier_attribute_wrapper(status_code, out_event_notifier)
+		return wrap.Read_event_notifier_attribute_wrapper(status_code, out_event_notifier)
 
 	def read_value_attribute(self, nodeId):
 		value = ffi.new("UA_Variant**")
 		status_code = lib.UA_Client_readValueAttribute(self.ua_client, nodeId, value.value)
-		return Read_value_attribute_wrapper
+		return wrap.Read_value_attribute_wrapper(status_code, value)
 
 	def read_data_type_attribute(self, node_id):
 		out_data_type = ffi.new("UA_NodeId**")
 		status_code = lib.UA_Client_readDataTypeAttribute(self.ua_client, node_id, out_data_type)
-		return Read_value_attribute_wrapper(status_code, out_data_type)
+		return wrap.Read_value_attribute_wrapper(status_code, out_data_type)
 
 	def read_value_rank_attribute(self, node_id):
 		out_value_rank = ffi.new("UA_Int32**")
 		status_code = lib.UA_Client_readValueRankAttribute(self.ua_client, node_id, out_value_rank)
-		return Read_value_attribute_wrapper(status_code, out_value_rank)
+		return wrap.Read_value_attribute_wrapper(status_code, out_value_rank)
 
 	def read_array_dimensions_attribute(self, node_id):
 		out_array_dimensions_size = ffi.new("size_t**")
 		out_array_dimensions = ffi.new("UA_UInt32***")
 		status_code = lib.UA_Client_readArrayDimensionsAttribute(self.ua_client, node_id, out_array_dimensions_size, out_array_dimensions)
-		return Read_array_dimensions_attribute_wrapper(status_code, out_array_dimensions_size, out_array_dimensions)
+		return wrap.Read_array_dimensions_attribute_wrapper(status_code, out_array_dimensions_size, out_array_dimensions)
 
 	def read_access_level_attribute(self, node_id):
 		out_access_level = ffi.new("UA_Byte**")
 		status_code = lib.UA_Client_readAccessLevelAttribute(self.ua_client, node_id, out_access_level)
-		return Read_access_level_attribute_wrapper(status_code, out_access_level)
+		return wrap.Read_access_level_attribute_wrapper(status_code, out_access_level)
 
 	def read_user_access_level_attribute(self, node_id):
 		out_user_access_level = ffi.new("UA_Byte**")
 		status_code = lib.UA_Client_readUserAccessLevelAttribute(self.ua_client, node_id, out_user_access_level)
-		return Read_user_access_level_attribute_wrapper(status_code, out_user_access_level)
+		return wrap.Read_user_access_level_attribute_wrapper(status_code, out_user_access_level)
 
 	def read_minimum_sampling_interval_attribute(self, node_id):
 		out_min_sampling_interval = ffi.new("UA_Double**")
 		status_code = lib.UA_Client_readMinimumSamplingIntervalAttribute(self.ua_client, node_id, out_min_sampling_interval)
-		return Read_minimum_sampling_interval_attribute_wrapper(status_code, out_min_sampling_interval)
+		return wrap.Read_minimum_sampling_interval_attribute_wrapper(status_code, out_min_sampling_interval)
 
 	def read_executable_attribute(self, node_id):
 		out_executable = ffi.new("UA_Boolean**")
 		status_code = lib.UA_Client_readExecutableAttribute(self.ua_client, node_id, out_executable)
-		return Read_executable_attribute_wrapper(status_code, out_executable)
+		return wrap.Read_executable_attribute_wrapper(status_code, out_executable)
 
 	def read_user_executable_attribute(self, node_id):
 		out_user_executable = ffi.new("UA_Boolean**")
 		status_code = lib.UA_Client_readUserExecutableAttribute(self.ua_client, node_id, out_user_executable)
-		return Read_user_executable_attribute_wrapper(status_code, out_user_executable)
+		return wrap.Read_user_executable_attribute_wrapper(status_code, out_user_executable)
 
 
 	# write service
@@ -206,10 +206,10 @@ class UaClient:
 	# misc service calls
 
 	def call(self, object_id, method_id, input_size, input):
-		output_size = ffi.new("")
-		output = ffi.new("")
+		output_size = ffi.new("size_t**")
+		output = ffi.new("UA_Variant ***")
 		status_code = lib.UA_Client_call(self.ua_client, object_id, method_id, input_size, input, output_size, output)
-		return Call_wrapper(status_code, output_size, output)
+		return wrap.Call_wrapper(status_code, output_size, output)
 
 	def add_reference(self, source_node_id, reference_type_id, is_forward, target_server_uri, target_node_id, target_node_class):
 		return lib.UA_Client_addReference(self.ua_client, source_node_id, reference_type_id, is_forward, target_server_uri, target_node_id, target_node_class)
@@ -224,44 +224,44 @@ class UaClient:
 	# add node services
 
 	def add_variable_node(self, requested_new_node_id, parent_node_id, reference_type_id, browse_name, type_definition, attr = DefaultAttributes.VARIABLE_ATTRIBUTES_DEFAULT):
-		out_new_node_id = ffi.new("")
+		out_new_node_id = ffi.new("UA_NodeId*")
 		status_code = lib.UA_Client_addVariableNode(self.ua_client, requested_new_node_id, parent_node_id, reference_type_id, browse_name, type_definition, attr, out_new_node_id);
-		return Add_node_wrapper(status_code, out_new_node_id)
+		return wrap.Add_node_wrapper(status_code, out_new_node_id)
 
 	def add_variable_type_node(self, requested_new_node_id, parent_node_id, reference_type_id, browse_name, attr = DefaultAttributes.VARIABLE_TYPE_ATTRIBUTES_DEFAULT):
-		out_new_node_id = ffi.new("")
+		out_new_node_id = ffi.new("UA_NodeId*")
 		status_code = lib.UA_Client_addVariableTypeNode(self.ua_client, requested_new_node_id, parent_node_id, reference_type_id, browse_name, attr, out_new_node_id)
-		return Add_node_wrapper(status_code, out_new_node_id)
+		return wrap.Add_node_wrapper(status_code, out_new_node_id)
 
 	def add_object_node(self, requested_new_node_id, parent_node_id, reference_type_id, browse_name, type_definition, attr = DefaultAttributes.OBJECT_ATTRIBUTES_DEFAULT):
-		out_new_node_id = ffi.new("")
+		out_new_node_id = ffi.new("UA_NodeId*")
 		status_code = lib.UA_Client_addObjectNode(self.ua_client, requested_new_node_id, parent_node_id, reference_type_id, browse_name, type_definition, attr, out_new_node_id)
-		return Add_node_wrapper(status_code, out_new_node_id)
+		return wrap.Add_node_wrapper(status_code, out_new_node_id)
 
 	def add_object_type_node(self, requested_new_node_id, parent_node_id, reference_type_id, browse_name, attr = DefaultAttributes.OBJECT_TYPE_ATTRIBUTES_DEFAULT):
-		out_new_node_id = ffi.new("")
+		out_new_node_id = ffi.new("UA_NodeId*")
 		status_code = lib.UA_Client_addObjectTypeNode(self.ua_client, requested_new_node_id, parent_node_id, reference_type_id, browse_name, attr, out_new_node_id)
-		return Add_node_wrapper(status_code, out_new_node_id)
+		return wrap.Add_node_wrapper(status_code, out_new_node_id)
 
 	def add_view_node(self, requested_new_node_id, parent_node_id, reference_type_id, browse_name, attr = DefaultAttributes.VIEW_ATTRIBUTES_DEFAULT):
-		out_new_node_id = ffi.new("")
+		out_new_node_id = ffi.new("UA_NodeId*")
 		status_code = lib.UA_Client_addViewNode(self.ua_client, requested_new_node_id, parent_node_id, reference_type_id, browse_name, attr, out_new_node_id)
-		return Add_node_wrapper(status_code, out_new_node_id)
+		return wrap.Add_node_wrapper(status_code, out_new_node_id)
 
 	def add_reference_type_node(self, requested_new_node_id, parent_node_id, reference_type_id, browse_name, attr = DefaultAttributes.REFERENCE_TYPE_ATTRIBUTES_DEFAULT):
-		out_new_node_id = ffi.new("")
+		out_new_node_id = ffi.new("UA_NodeId*")
 		status_code = lib.UA_Client_addReferenceTypeNode(self.ua_client, requested_new_node_id, parent_node_id, reference_type_id, browse_name, attr, out_new_node_id)
-		return Add_node_wrapper(status_code, out_new_node_id)
+		return wrap.Add_node_wrapper(status_code, out_new_node_id)
 
 	def add_data_type_node(self, requested_node_id, parent_node_id, reference_type_id, browse_name, attr = DefaultAttributes.DATA_TYPE_ATTRIBUTES_DEFAULT):
-		out_new_node_id = ffi.new("")
+		out_new_node_id = ffi.new("UA_NodeId*")
 		status_code = lib.UA_Client_addDataTypeNode(self.ua_client, requested_node_id, parent_node_id, reference_type_id, browse_name, attr, out_new_node_id)
-		return Add_node_wrapper(status_code, out_new_node_id)
+		return wrap.Add_node_wrapper(status_code, out_new_node_id)
 
 	def add_method_node(self, requested_new_node_id, parent_node_id, reference_type_id, browse_name, attr = DefaultAttributes.METHOD_ATTRIBUTED_DEFAULT):
-		out_new_node_id = ffi.new("")
+		out_new_node_id = ffi.new("UA_NodeId*")
 		status_code = lib.UA_Client_addMethodNode(self.ua_client, requested_new_node_id, parent_node_id, reference_type_id, browse_name, attr, out_new_node_id)
-		return Add_node_wrapper(status_code, out_new_node_id)
+		return wrap.Add_node_wrapper(status_code, out_new_node_id)
 
 	# utils
 
