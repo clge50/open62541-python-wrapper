037d06da9545a4dbd2d563e1800de01afc82bdf3
Christian lange <christian.lange@mni.thm.de>
2021-01-29 01:49:55 +0100

    improved clientApi

--- examples/client_basic_with_newfuns.py
+++ examples/client_basic_with_newfuns.py
@@ -9,14 +9,14 @@ client = clientApi.UaClient()
 retval = client.connect(b"opc.tcp://127.0.0.1:4840/")
 
 # adding node
-myIntegerNodeId = lib.UA_NODEID_STRING(1, b"the.answer");
-myIntegerName = lib.UA_QUALIFIEDNAME(1, b"the answer");
+myIntegerNodeId = lib.UA_NODEID_STRING(1, b"the.answer")
+myIntegerName = lib.UA_QUALIFIEDNAME(1, b"the answer")
 parentNodeId = lib.UA_NODEID_NUMERIC(ffi.cast("UA_UInt16", 0), ffi.cast("UA_UInt32", 85))
 parentReferenceNodeId = lib.UA_NODEID_NUMERIC(ffi.cast("UA_UInt16", 0), ffi.cast("UA_UInt32", 35))
 variableType = lib.UA_NODEID_NUMERIC(0, 63)
 wrapper = client.add_variable_node(myIntegerNodeId, parentNodeId, parentReferenceNodeId, myIntegerName, variableType)
-print("Status code: " + str(wrapper.status_code))
-print("Node id: " + wrapper.out_new_node_id.identifier.string)
+print("Status code is bad: " + str(lib.UA_StatusCode_isBad(wrapper.status_code)))
+# print("Node id: " + wrapper.out_new_node_id.identifier.string.data)
 
 # reading node
 myIntNodeIdResult = client.read_node_id_attribute(wrapper.out_new_node_id[0])
---
+++ src/api/client_service_results.py
@@ -0,0 +1,144 @@
+# read service
+
+class ReadNodeIdAttributeResult:
+    def __init__(self, status_code, out_node_id):
+        self.status_code = status_code
+        self.out_node_id = out_node_id
+
+
+class ReadNodeClassAttributeResult:
+    def __init__(self, status_code, out_node_id):
+        self.status_code = status_code
+        self.out_node_id = out_node_id
+
+
+class ReadBrowseNameAttributeResult:
+    def __init__(self, status_code, out_browse_name):
+        self.status_code = status_code
+        self.out_browse_name = out_browse_name
+
+
+class ReadDisplayNameAttributeResult:
+    def __init__(self, status_code, out_display_name):
+        self.status_code = status_code
+        self.out_display_name = out_display_name
+
+
+class ReadDescriptionAttributeResult:
+    def __init__(self, status_code, out_description):
+        self.status_code = status_code
+        self.out_description = out_description
+
+
+class ReadWriteMaskAttributeResult:
+    def __init__(self, status_code, out_write_mask):
+        self.status_code = status_code
+        self.out_write_mask = out_write_mask
+
+
+class ReadUserWriteMaskAttributeResult:
+    def __init__(self, status_code, out_user_write_mask):
+        self.status_code = status_code
+        self.out_user_write_mask = out_user_write_mask
+
+
+class ReadIsAbstractAttributeResult:
+    def __init__(self, status_code, out_is_abstract):
+        self.status_code = status_code
+        self.out_is_abstract = out_is_abstract
+
+
+class ReadSymmetricAttributeResult:
+    def __init__(self, status_code, out_symmetric):
+        self.status_code = status_code
+        self.out_symmetric = out_symmetric
+
+
+class ReadInverseNameAttributeResult:
+    def __init__(self, status_code, out_inverse_name):
+        self.status_code = status_code
+        self.out_inverse_name = out_inverse_name
+
+
+class ReadContainsNoLoopsAttributeResult:
+    def __init__(self, status_code, out_contains_no_loops):
+        self.status_code = status_code
+        self.out_contains_no_loops = out_contains_no_loops
+
+
+class ReadEventNotifierAttributeResult:
+    def __init__(self, status_code, out_event_notifier):
+        self.status_code = status_code
+        self.out_event_notifier = out_event_notifier
+
+
+class ReadValueAttributeResult:
+    def __init__(self, status_code, value):
+        self.status_code = status_code
+        self.value = value
+
+
+class ReadDataTypeAttribute:
+    def __init__(self, status_code, out_data_type):
+        self.status_code = status_code
+        self.value = out_data_type
+
+
+class ReadValueRankAttribute:
+    def __init__(self, status_code, out_value_rank):
+        self.status_code = status_code
+        self.value = out_value_rank
+
+
+class ReadArrayDimensionsAttributeResult:
+    def __init__(self, status_code, out_array_dimensions_size, out_array_dimensions):
+        self.status_code = status_code
+        self.out_array_dimensions_size = out_array_dimensions_size
+        self.out_array_dimensions = out_array_dimensions
+
+
+class ReadAccessLevelAttributeResult:
+    def __init__(self, status_code, out_access_level):
+        self.status_code = status_code
+        self.out_access_level = out_access_level
+
+
+class ReadUserAccessLevelAttributeResult:
+    def __init__(self, status_code, out_user_access_level):
+        self.status_code = status_code
+        self.out_user_access_level = out_user_access_level
+
+
+class ReadMinimumSamplingIntervalAttributeResult:
+    def __init__(self, status_code, out_min_sampling_interval):
+        self.status_code = status_code
+        self.out_min_sampling_interval = out_min_sampling_interval
+
+
+class ReadExecutableAttributeResult:
+    def __init__(self, status_code, out_executable):
+        self.status_code = status_code
+        self.out_executable = out_executable
+
+
+class ReadUserExecutableAttributeResult:
+    def __init__(self, status_code, out_user_executable):
+        self.status_code = status_code
+        self.out_user_executable = out_user_executable
+
+
+# misc service
+
+class CallResult:
+    def __init__(self, status_code, output_size, output):
+        self.status_code = status_code
+        self.output_size = output_size
+        self.output = output
+
+
+# add node service
+
+class Add_node_result:
+    def __init__(self, status_code, out_new_node_id):
+        self.status_code = status_code
+        self.out_new_node_id = out_new_node_id
