eb67291aaf3da4016b82def9bad689affcd91ef6
Christian lange <christian.lange@mni.thm.de>
2021-03-04 14:55:09 +0100

    small fixes

--- examples/types_example_new_async.py
+++ examples/types_example_new_async.py
@@ -2,14 +2,12 @@ import sys
 import time
 
 sys.path.append("../build/open62541")
-import clientApi
-from ua_types import *
-from intermediateApi import ffi, lib
+from ua import *
 
 x = UaStatusCode(0x80000000)
 print(x.is_bad())
 
-client = clientApi.UaClient()
+client = UaClient()
 retval = client.connect("opc.tcp://127.0.0.1:4840/")
 
 # configuring attribute
@@ -23,16 +21,18 @@ retval = client.connect("opc.tcp://127.0.0.1:4840/")
 # adding node
 my_integer_node_id = UaNodeId(1, "the answer")
 my_integer_name = UaQualifiedName(1, "the.answer")
-parent_node_id = UaNodeId(0, UaNodeId.UA_NS0ID_OBJECTSFOLDER)
-parent_reference_node_id = UaNodeId(0, UaNodeId.UA_NS0ID_ORGANIZES)
-variable_type = UaNodeId(0, UaNodeId.UA_NS0ID_BASEDATAVARIABLETYPE)
+parent_node_id = NS0ID.OBJECTSFOLDER
+parent_reference_node_id = NS0ID.ORGANIZES
+variable_type = NS0ID.BASEDATAVARIABLETYPE
 
 parent_node_read_result = client.read_node_id_attribute_async(parent_reference_node_id,
                                                               lambda _client, req_id, out,
                                                                      user_data="async test 1": print(
                                                                   f"Node id: {str(out)}, request-id: {str(req_id)}, user_data: {user_data}"))
 print("req_id: " + str(parent_node_read_result.req_id))
-client.run_iterate(1)
+print("sleeping 2s and then iterate")
+time.sleep(2)
+client.run_iterate(UaUInt32(1))
 print("parent node read status code: " + str(parent_node_read_result.status_code))
 
 add_variable_node_result = client.add_variable_node_async(my_integer_node_id,
@@ -42,4 +42,6 @@ add_variable_node_result = client.add_variable_node_async(my_integer_node_id,
                                                           variable_type,
                                                           lambda _client, req_id, ar, user_data="async test 2": print(
                                                               f"Request-id: {str(req_id)}, user_data: {user_data}"))
-client.run_iterate(1)
+print("sleeping 2s and then iterate")
+time.sleep(2)
+client.run_iterate(UaUInt32(1))
